dd5cda82ed856e5895b24d9e78ee3a7364da2201
Alessandro <4143332+AlessandroMinali@users.noreply.github.com>
2021-02-19 14:29:15 -0800

    support displaying songs with kanji

--- music.5s.sh
+++ music.5s.sh
@@ -1,4 +1,5 @@
 #!bin/bash
 
 # Show music playing from my custom music program: https://alessandrominali.github.io/tiny_music_player.html
+export LANG="ja_JP.UTF-8"
 ps u | grep afplay | grep -v grep | rev | cut -d/ -f1 |  cut -d- -f2,3,4,5,6 | rev
