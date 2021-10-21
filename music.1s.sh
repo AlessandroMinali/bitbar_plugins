39dae591b1f4d24f9beb70ac95daec48ed4bf571
Alessandro <4143332+AlessandroMinali@users.noreply.github.com>
2021-10-20 21:03:58 -0400

    trunc

--- music.1s.sh
+++ music.1s.sh
@@ -3,5 +3,5 @@
 # Show music playing from my custom music program: https://alessandrominali.github.io/tiny_music_player.html
 export LANG="ja_JP.UTF-8"
 if player=$(ps u | grep afplay | grep -v grep); then
-  echo $player | rev | cut -d/ -f1 | cut -d. -f2-10 | rev
+  echo $player | rev | cut -d/ -f1 | cut -d. -f2-10 | rev | cut -c 10
 fi
