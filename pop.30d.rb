4f82fb1e7bd8af90d69eef1c96e5d0eabd72ad11
Alessandro <4143332+AlessandroMinali@users.noreply.github.com>
2022-07-31 12:12:41 -0600

    world pop tracking

---
+++ pop.30d.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/ruby
+require 'net/http'
+
+uri = URI('https://www.worldometers.info/world-population/')
+print (9.73 - Net::HTTP.get(uri).match(/<title>.+(\d\.\d\d).+<\/title>/)[1].to_f).round(2), 'B'
