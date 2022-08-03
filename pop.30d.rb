e5a51ebb5d50e50d8ebf61fd6827717863af8a38
Alessandro <4143332+AlessandroMinali@users.noreply.github.com>
2022-08-03 10:53:37 -0600

    sleep

--- pop.30d.rb
+++ pop.30d.rb
@@ -2,4 +2,9 @@
 require 'net/http'
 
 uri = URI('https://www.worldometers.info/world-population/')
-print (9.73 - Net::HTTP.get(uri).match(/<title>.+(\d\.\d\d).+<\/title>/)[1].to_f).round(2), 'B' rescue retry
+begin
+  print (9.73 - Net::HTTP.get(uri).match(/<title>.+(\d\.\d\d).+<\/title>/)[1].to_f).round(2), 'B'
+rescue
+  sleep 1800
+  retry
+end
