21ca80ef1e163090e063eee3804f87a43361024d
AlessandroMinali <4143332+AlessandroMinali@users.noreply.github.com>
2024-05-02 08:13:38 -0400

    better parse

--- music.1s.sh
+++ music.1s.sh
@@ -3,5 +3,5 @@
 # Show music playing from my custom music program: https://alessandrominali.github.io/tiny_music_player.html
 export LANG="ja_JP.UTF-8"
 if player=$(ps ux | grep afplay | grep -v grep); then
-  echo $player | rev | cut -d/ -f1 | rev| cut -d. -f1 | cut -c 1-30
+  echo $player | rev | cut -d/ -f1 | cut -d. -f3 | rev | cut -c 1-30
 fi
